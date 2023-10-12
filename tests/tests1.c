33864de1201a1abcb489bb5ac717dea8f81ce2d4
Thomas Boos <tboos70@gmail.com>
2023-10-12 21:36:30 +0200

    reorganized unit test

--- tests/tests1.c
+++ tests/tests1.c
@@ -22,27 +22,27 @@ CU_TEST_TEARDOWN() {
 }
  
 /* Test that two is bigger than one */
-static void test_bytesize_pass(void) {
+static void test_typesize_pass(void) {
     CU_ASSERT_FATAL(sizeof(byte_t) == 1);
+    CU_ASSERT_FATAL(sizeof(word_t) == 2);
+    CU_ASSERT_FATAL(sizeof(dword_t) == 4);
+    CU_ASSERT_FATAL(BLOCKSIZE % sizeof(dinode_t) == 0);
 }
  
 /* Test that two is bigger than one */
 static void test_wordsize_pass(void) {
-    CU_ASSERT_FATAL(sizeof(word_t) == 2);
 }
  
 /* Test that two is bigger than one */
 static void test_dwordsize_pass(void) {
-    CU_ASSERT_FATAL(sizeof(dword_t) == 4);
 }
 
 /* Test that two is bigger than one */
 static void test_dinodesize_pass(void) {
-    CU_ASSERT_FATAL(sizeof(dinode_t) == 64);
 }
  
 CUNIT_CI_RUN("my-suite",
-             CUNIT_CI_TEST(test_bytesize_pass),
+             CUNIT_CI_TEST(test_typesize_pass),
              CUNIT_CI_TEST(test_wordsize_pass),
              CUNIT_CI_TEST(test_dwordsize_pass),
              CUNIT_CI_TEST(test_dinodesize_pass)
