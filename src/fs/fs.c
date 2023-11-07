357e66958f29cc48086d553451d72aa1ce691a9c
Thomas Boos <tboos70@gmail.com>
2023-11-07 19:34:45 +0000

    rmdir + refactoring in fs.c

--- src/fs/fs.c
+++ src/fs/fs.c
@@ -15,6 +15,7 @@
 #include "inode.h"
 #include "utils.h"
 #include "buf.h"
+#include "pc.h"
 
 
 
@@ -30,44 +31,67 @@ const char *basename(const char *path)
 }
 
 
-
-void unlink(const char *path)
+void unlinki(iinode_t *ipdir, const char *bname)
 {
-  ASSERT(path != NULL);
-  namei_t in = namei(path);
-  iinode_t *pi;   // parent inode
-
-  if (in.i == NULL || in.p == 0) {
-    if (in.i != NULL)
-      iput(in.i);
-    return;
-  }
-
-  pi = iget(in.i->fs, in.p);
-  ASSERT(pi != NULL);
-  ASSERT(pi->dinode.ftype == DIRECTORY);
-  ASSERT(pi->dinode.fsize % sizeof(dirent_t) == 0);
-  int n = pi->dinode.fsize / sizeof(dirent_t);
-  const char *bn = basename(path);
-  int bl = snlen(bn, sizeof(DIRNAMEENTRY));
+  ASSERT(ipdir);
+  ASSERT(bname);
+  ASSERT(ipdir->dinode.ftype == DIRECTORY);
+  ASSERT(ipdir->dinode.fsize % sizeof(dirent_t) == 0);
+  int n = ipdir->dinode.fsize / sizeof(dirent_t);
+  int bl = snlen(bname, sizeof(DIRNAMEENTRY));
   for (int i = 0; i < n; i++) {
-    bmap_t b = bmap(pi, i * sizeof(dirent_t));
-    ASSERT(b.fsblock != 0);
-    bhead_t *bh = bread(LDEVFROMINODE(pi), b.fsblock);
-    dirent_t *de = (dirent_t *)bh->buf->mem;
-    if (sncmp(de->name, bn, bl) == 0) {
-      ASSERT(de->inum == in.i->inum);
+    bmap_t b = bmap(ipdir, i * sizeof(dirent_t));
+    ASSERT(b.fsblock > 0);
+    bhead_t *bh = bread(LDEVFROMINODE(ipdir), b.fsblock);
+    dirent_t *de = (dirent_t *)&bh->buf->mem[b.offblock];
+    if (sncmp(de->name, bname, bl) == 0) {
+      iinode_t *ii = iget(ipdir->fs, de->inum);
+      if (ii == NULL) {
+        brelse(bh);
+        return;
+      }
+      while(ii->locked) {
+        waitfor(INODELOCKED);
+      }
+      ii->locked = true;
+      ii->dinode.nlinks--;
+      ii->modified = true;
+      ii->locked = false;
+      wakeall(INODELOCKED);
+      iput(ii);
       de->inum = 0;
-      in.i->dinode.nlinks--;
-      in.i->modified = true;
       bh->dwrite = true;
       bwrite(bh);
       brelse(bh);
       break;
     }
+    brelse(bh);
   }
-  iput(pi);
+}
+
+
+
+void unlink(const char *path)
+{
+  ASSERT(path != NULL);
+  namei_t in = namei(path);
+  if (in.i == NULL || in.p == 0) {
+    if (in.i != NULL)
+      iput(in.i);
+    return;
+  }
+  if (in.i->dinode.ftype == DIRECTORY) {
+    /// @todo error is directory
+    iput(in.i);
+    return;
+  }
   iput(in.i);
+  iinode_t *pi = iget(in.fs, in.p);
+  if (pi == NULL)
+    return;   // error already set by iget
+  ASSERT(pi->dinode.ftype == DIRECTORY);
+  unlinki(pi, basename(path));
+  iput(pi);
 }
 
 
@@ -105,18 +129,31 @@ void linki(iinode_t *ii, const char *newpath)
     if (de->inum == 0) {
       de->inum = ii->inum;
       sncpy(de->name, basename(newpath), sizeof(DIRNAMEENTRY));
+      while (ii->locked) {
+        waitfor(INODELOCKED);
+      }
+      ii->locked = true;      
       ii->dinode.nlinks++;
       ii->modified = true;
+      ii->locked = false;
+      wakeall(INODELOCKED);
       bh->dwrite = true;
       bwrite(bh);
       brelse(bh);
       if (i == n) { // new directory entry
+        while(pi->locked) {
+          waitfor(INODELOCKED);
+        }
+        pi->locked = true;
         pi->dinode.fsize += sizeof(dirent_t);
         pi->modified = true;
+        pi->locked = false;
+        wakeall(INODELOCKED);
       }
       /// @todo reset error if necessary
       break;
     }
+    brelse(bh);
   }
   iput(pi);
 }
@@ -133,21 +170,25 @@ void link(const char *path, const char *newpath)
       iput(in.i);
     return;   // error already set by namei
   }
+  if (in.i->dinode.ftype == DIRECTORY) {
+    /// @todo error is directory
+    iput(in.i);
+    return;
+  }
+  
   linki(in.i, newpath);
   iput(in.i);
 }
 
 
 
-void mkdir(const char *path)
+void mknode(const char *path, ftype_t ftype)
 {
-  char sbuf[MAXPATH];
-
   ASSERT(path != NULL);
+  ASSERT(ftype >= REGULAR && ftype <= FIFO);
   namei_t in = namei(path);
-  if (in.i != NULL)
-    iput(in.i);
   if (in.i != NULL) {
+    iput(in.i);
     /// @todo error link already exists
     return;
   }
@@ -157,20 +198,100 @@ void mkdir(const char *path)
   if (pi == NULL)
     return;   // error already set by iget
 
-  iinode_t *ii = ialloc(in.fs, DIRECTORY);
-  if (ii == NULL) 
+  iinode_t *ii = ialloc(in.fs, ftype);
+  if (ii == NULL) {
+    iput(pi);
     return;   // error already set by ialloc
+  }
 
   linki(ii, path);
-  sncpy(sbuf, path, MAXPATH);
-  snapnd(sbuf,"/.", MAXPATH);
-  linki(ii, sbuf);
-  sncpy(sbuf, path, MAXPATH);
-  snapnd(sbuf,"/..", MAXPATH);
-  linki(pi, sbuf);
   iput(ii);
   iput(pi);
 }
 
 
 
+void mkdir(const char *path)
+{
+  ASSERT(path != NULL);
+  mknode(path, DIRECTORY);
+  namei_t in = namei(path);
+  if (in.i == NULL) {
+    /// @todo error link does not exists
+    return;
+  }
+  if (in.p == 0)    // parent dir not found
+    return;   // error already set by namei
+  iinode_t *pi = iget(in.fs, in.p);
+  if (pi == NULL)
+    return;   // error already set by iget
+
+  bmap_t b = bmap(in.i, 0);
+  if (b.fsblock == 0) {  // no new blocks left on device
+    unlinki(pi, basename(path));
+    iput(in.i);
+    iput(pi);
+    return;
+  }
+  bhead_t *bh = bread(LDEVFROMINODE(in.i), b.fsblock);
+  dirent_t *de = (dirent_t *)bh->buf->mem;
+  de[0].inum = in.i->inum;
+  sncpy(de[0].name, ".", sizeof(DIRNAMEENTRY));
+  de[1].inum = pi->inum;
+  sncpy(de[1].name, "..", sizeof(DIRNAMEENTRY));
+  while(in.i->locked) {
+    waitfor(INODELOCKED);
+  }
+  in.i->locked = true;
+  in.i->dinode.fsize = 2 * sizeof(dirent_t);
+  in.i->dinode.nlinks++;
+  in.i->modified = true;
+  in.i->locked = false;
+  wakeall(INODELOCKED);
+  while(pi->locked) {
+    waitfor(INODELOCKED);
+  }
+  pi->locked = true;
+  pi->dinode.nlinks++;
+  pi->modified = true;
+  pi->locked = false;
+  wakeall(INODELOCKED);
+  bh->dwrite = true;
+  bwrite(bh);
+  brelse(bh);
+  iput(in.i);
+  iput(pi);
+}
+
+
+
+void rmdir(const char *path)
+{
+  ASSERT(path != NULL);
+  namei_t in = namei(path);
+  if (in.i == NULL || in.p == 0) {
+    if (in.i != NULL)
+      iput(in.i);
+    return;   // error already set by namei
+  }
+  if (in.i->dinode.ftype != DIRECTORY) {
+    /// @todo error is not directory
+    iput(in.i);
+    return;
+  }
+  if (in.i->dinode.nlinks > 3) {
+    /// @todo error directory not empty
+    iput(in.i);
+    return;
+  }
+  iinode_t *pi = iget(in.fs, in.p);
+  if (pi == NULL) {
+    iput(in.i);
+    return;   // error already set by iget
+  }
+  unlinki(in.i, ".");
+  unlinki(in.i, "..");
+  iput(in.i);
+  unlinki(pi, basename(path));
+  iput(pi);
+}
